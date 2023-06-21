ccdcf143b616835b8f06bfa0d0051e993e9a8b61
ana710 <khangtran7290@gmail.com>
2023-06-22 00:51:50 +0700

    '22/6'

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';  
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  @Output() featureSelected = new EventEmitter<string>();
+ constructor() { }
+ ngOnInit(): void {
+ }
+ onSelect(feature: string) {
+ this.featureSelected.emit(feature);
+ }
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Recipe } from './../recipe.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 @Component({
  selector: 'app-recipe-list',
  templateUrl: './recipe-list.component.html',
@@ -11,8 +11,13 @@ export class RecipeListComponent implements OnInit {
     new Recipe('A Test Recipe 2', 'This is simply a test 2', 'https://assets.bonappetit.com/photos/5c2f8fe26558e92c8a622671/1:1/w_2700,h_2700,c_limit/bolognese-1.jpg'),
     new Recipe('A Test Recipe 3', 'This is simply a test 3', 'https://assets.bonappetit.com/photos/5c2f8fe26558e92c8a622671/1:1/w_2700,h_2700,c_limit/bolognese-1.jpg')
   ];
+    @Output() recipeWasSelected = new EventEmitter<Recipe>();
     constructor() { }
     ngOnInit(): void {
+    }
+    onRecipeSelected(recipe: Recipe)
+   {
+    this.recipeWasSelected.emit(recipe);
     }
    }
 
